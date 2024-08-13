c47685bcf1c54eb2214b9b25e660b07f01d4bbe2
yasmeenodeh <odehyasmeen22@gmail.com>
2024-08-13 23:27:39 +0300

    yasmeen_15/8

--- New folder/untitled3/src/test/java/AcceptanceTest.java
+++
@@ -1,9 +0,0 @@
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "fear", plugin = {"html:target/cucumber/wikipedia.html"}, monochrome = true, snippets = CucumberOptions.SnippetType.CAMELCASE)
-public class
-AcceptanceTest {
-}

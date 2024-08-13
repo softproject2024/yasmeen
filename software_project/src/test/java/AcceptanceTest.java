4a231a10f541ddac021f48e08ddc8117530786e4
yasmeenodeh <odehyasmeen22@gmail.com>
2024-08-13 23:26:05 +0300

    yasmeen_13/8

--- software_project/src/test/java/AcceptanceTest.java
+++ software_project/src/test/java/AcceptanceTest.java
@@ -3,6 +3,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "fear", plugin = {"html:target/cucumber/wikipedia.html"}, monochrome = true, snippets = CucumberOptions.SnippetType.CAMELCASE)
+@CucumberOptions(features = "Features", plugin = {"html:target/cucumber/wikipedia.html"}, monochrome = true, snippets = CucumberOptions.SnippetType.CAMELCASE)
 public class AcceptanceTest {
 }

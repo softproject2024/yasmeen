6aa981f36201d71c6a4704b053d8775f59fb5bd5
yasmeenodeh <odehyasmeen22@gmail.com>
2024-08-23 16:56:49 +0300

    Update Owneraccount.java

--- src/main/java/yasmeen/softwareproject/Owneraccount.java
+++ src/main/java/yasmeen/softwareproject/Owneraccount.java
@@ -6,22 +6,13 @@ package yasmeen.softwareproject;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Hp
- */
+
 public class Owneraccount extends Account{
-    private ArrayList<Store> stores;
+
     public Owneraccount(String name, int age, String email, int phone,int password) {
         super(name, age, email, phone,"Owner",password);
-        stores=new ArrayList<>();
+
     }
-    public void addstore(String storename,String cityname){
-       stores.add(new Store(storename,cityname));
-    }
-public ArrayList<Store> getStores(){
-        return stores;
-}
 
 
 }

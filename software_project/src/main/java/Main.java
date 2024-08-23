2387cbd7ac26c6155db74f5266945fa8521a0d85
yasmeenodeh <odehyasmeen22@gmail.com>
2024-08-23 14:29:10 +0300

    ad

--- software_project/src/main/java/Main.java
+++ software_project/src/main/java/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("i am sld,yen");
+        System.out.println("i am yasmeen");
     }
 }
